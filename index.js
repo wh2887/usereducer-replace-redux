b3f99a3cdff1df34976b547cdd6cb941599802b5
McCallWang <13567272790@163.com>
2020-10-28 15:55:31 +0800

    创建对数据读写的 API

--- index.js
+++ index.js
@@ -3,9 +3,33 @@ import ReactDOM from 'react-dom';
 import Books from './components/books';
 import Movies from './components/movies';
 import User from './components/user';
+import userReducer from './src/reducers/user_reducer'
+import moviesReducer from './src/reducers/movies_reducer'
+import booksReducer from './src/reducers/books_reducer'
+import stores from './src/stores/index'
+
+
+const obj = {
+  ...userReducer,
+  ...booksReducer,
+  ...moviesReducer
+}
+
+const reducer = (state, action) => {
+  const fn = obj[action.type]
+  if (fn) {
+    return fn(state, action)
+  } else {
+    throw new Error('unknown type!')
+  }
+}
+
 
 
 const App = () => {
+
+  const [state, dispatch] = useReducer(reducer, stores)
+
   return (
     <div>
       <h1>如何使用 useReducer 代替 Redux</h1>
